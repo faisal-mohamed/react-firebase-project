62f464f14b490d18844c7ed4afcd5a2e6df40365
Mohamed Faisal <97388703+faisal-mohamed@users.noreply.github.com>
2023-07-16 20:25:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Main } from './pages/Main';
+import { Login } from './pages/Login';
+import { Navbar } from './components/Navbar';
+import { CreatePost } from './pages/posts/CreatePost';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/create-post' element={<CreatePost/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
